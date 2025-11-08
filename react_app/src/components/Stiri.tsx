6d395a2b89252e1cfdecd166afc96dc47a854a44
valentincostea180 <152385738+valentincostea180@users.noreply.github.com>
2025-11-08 22:59:41 +0200

    changed several language misalignments

--- react_app/src/components/Stiri.tsx
+++ react_app/src/components/Stiri.tsx
@@ -193,7 +193,7 @@ const Stiri: React.FC<Props> = ({ jsonUrl, path }) => {
                   }
                 }}
               >
-                Adaugă
+                Add
               </button>
             </div>
 
